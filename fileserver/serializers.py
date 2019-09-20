ac1a8da88227345ba83e64fe1e849909e9120db0
heping945 <602013597@qq.com>
2019-09-21 02:23:33 +0800

    图片重复的更新 （md)检查省空间

--- fileserver/serializers.py
+++ fileserver/serializers.py
@@ -1,34 +1,32 @@
 from rest_framework import serializers
 
 from .models import PostImg
-from .utils import getFileMd5,getFileSize,judgeFileType,rename
-
-
+from .utils import getFileMd5, getFileSize, judgeFileType, rename
 
 
 class PostImgSerializers(serializers.ModelSerializer):
     class Meta:
         model = PostImg
         fields = '__all__'
-        read_only_fields = ('imgMd5','name')
+        read_only_fields = ('imgMd5', 'name')
 
     def validate(self, attrs):
         img = attrs['img']
-        # 判断图片是否存在
         md5 = getFileMd5(img)
-        imgobj = PostImg.objects.filter(imgMd5=md5)
-        # 省略 暂时没用
-        # if imgobj:
-        #     raise serializers.ValidationError('文件已存在')
-        # 重命名
         name = rename(img.name)
         # 判断图片大小
         file_size = getFileSize(img.size)
         if not file_size:
             raise serializers.ValidationError('图片太大')
-        file_md = getFileMd5(img)
-        print(md5)
         attrs['imgMd5'] = md5
         attrs['name'] = name
-        print(attrs, 'hello')
         return attrs
+
+    def create(self, validated_data):
+        img = validated_data['img']
+        # 判断图片是否存在
+        md5 = getFileMd5(img)
+        imgobj = PostImg.objects.filter(imgMd5=md5)
+        if imgobj:
+            return PostImg.objects.filter(imgMd5=md5).first()
+        return PostImg.objects.create(**validated_data)
