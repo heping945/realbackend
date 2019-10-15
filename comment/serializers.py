c88c3bc2a0e02cb89e8109fb8a26c153ba8dab9e
heping945 <602013597@qq.com>
2019-10-16 05:53:27 +0800

    评论模块完善。。1016

--- comment/serializers.py
+++ comment/serializers.py
@@ -45,10 +45,10 @@ class CommentSubSerialziser(serializers.ModelSerializer):
     author = CommentUserSerialziser()
     create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
     reply_to = ReplyCommentSerializer()
-    parent_id = serializers.CharField(source='parent_comment_id')
+    parent_comment = serializers.CharField(source='parent_comment_id')
     class Meta:
         model = Comment
-        fields = ('id', 'content', 'author', 'reply_to','create_time','parent_id')
+        fields = ('id', 'content', 'author', 'reply_to','create_time','parent_comment')
 
 
 # （id，内容，文章，作者，创建事件，子评论，父评论）----->(对应的是 get_object)
@@ -65,11 +65,12 @@ class CommentSerialziser(serializers.ModelSerializer):
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
-    # parent_comment=CommentSubSerialziser()
-    # reply_to =
+    author = CommentUserSerialziser(read_only=True)
+    create_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
+    sub_comments = CommentSubSerialziser(many=True,read_only=True)
     class Meta:
         model = Comment
-        fields = ('id', 'content', 'post', 'reply_to', 'parent_comment')
+        fields = ('id', 'content', 'post', 'reply_to', 'parent_comment','author','create_time','sub_comments')
 
 
 # 以下是反馈消息序列化
